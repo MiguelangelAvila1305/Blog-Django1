c69d96528db2e3277780d58b0fef8cc2c83d7dcd
Miguelangel Avila <miguelangel.a1305@gmail.com>
2023-05-06 16:58:05 -0400

    Post detail added

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 # Create your views here.
 
 class BlogListView(ListView):
     model= Post
     template_name= 'home.html'
+
+class BlogDetailView(DetailView):
+    model= Post
+    template_name='post_detail.html'
+
